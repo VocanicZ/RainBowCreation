ab694a23e008ee4bb66ebf505de694b262382b7b
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2022-12-31 18:49:26 +0700

    add config.yml and build num

--- src/main/java/me/vocanicz/rainbowcreation/Rainbowcreation.java
+++ src/main/java/me/vocanicz/rainbowcreation/Rainbowcreation.java
@@ -2,11 +2,14 @@ package me.vocanicz.rainbowcreation;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public final class Rainbowcreation extends JavaPlugin {
+    public static final String BUILD_NUMBER = "2";
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        this.saveDefaultConfig();
 
     }
 
