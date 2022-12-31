8de2df16daceae2cc8b44b565605cd2e39917691
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2022-12-31 16:35:13 +0700

    empty project

---
+++ src/main/java/me/vocanicz/rainbowcreation/Rainbowcreation.java
@@ -0,0 +1,17 @@
+package me.vocanicz.rainbowcreation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Rainbowcreation extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
